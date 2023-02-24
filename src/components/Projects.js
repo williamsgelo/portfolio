8b410bff0e653657f97a0fc24d6199dfa858910c
angelo <angelowilliams1015@gmail.com>
2023-02-24 16:40:35 +0200

    first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,134 @@
+import { Col, Container, Tab, Row, Nav } from 'react-bootstrap';
+import { ProjectCard } from './ProjectCard';
+import colorSharp2 from '../assets/img/color-sharp2.png';
+import projImg1 from '../assets/img/project-img1.png';
+// import projImg2 from '../assets/img/project-img2.png';
+// import projImg3 from '../assets/img/project-img3.png';
+import projImg4 from '../assets/img/project-img4.png';
+import projImg5 from '../assets/img/project-img5.png';
+import projImg6 from '../assets/img/project-img6.png';
+import projImg7 from '../assets/img/project-img7.png';
+import projImg8 from '../assets/img/project-img8.png';
+import projImg9 from '../assets/img/project-img9.png';
+import projImg10 from '../assets/img/project-img10.png';
+import projImg11 from '../assets/img/project-img11.png';
+
+
+
+
+export const Projects = () => {
+
+    const projects = [
+        {
+            title: "GPT3",
+            description: "",
+            imgUrl: projImg9,
+            link: "https://williamsgelo.github.io/gpt3",
+        },
+        {
+            title: "Movie UI",
+            description: "",
+            imgUrl: projImg1,
+            link: "https://williamsgelo.github.io/movieapp",
+        },
+        {
+            title: "Memory Game",
+            description: "",
+            imgUrl: projImg4,
+            link: "https://williamsgelo.github.io/magic-memory",
+        },
+        {
+            title: "Stripe Menu",
+            description: "",
+            imgUrl: projImg5,
+            link: "https://williamsgelo.github.io/stripe-menu",
+        },
+        {
+            title: "Live User Filter",
+            description: "",
+            imgUrl: projImg6,
+            link: "https://williamsgelo.github.io/live-user-filter"
+        },
+        {
+            title: "Hacker News",
+            description: "",
+            imgUrl: projImg7,
+            link: "https://williamsgelo.github.io/hacker-news",
+        },
+        {
+            title: "Tindog",
+            description: "",
+            imgUrl: projImg8,
+            link: "https://williamsgelo.github.io/tindog-main",
+        },
+        {
+            title: "Restaurant Menu",
+            description: "",
+            imgUrl: projImg11,
+            link: "https://williamsgelo.github.io/restaurant-menu",
+        },
+        {
+            title: "Quiz App",
+            description: "",
+            imgUrl: projImg10,
+            link: "https://williamsgelo.github.io/quizapp",
+        }
+
+    ];
+
+
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col size={12}>
+                        <h2>Projects</h2>
+                        <p>This is projects that I have individually worked on and with some team members. All my projects use the latest web technologies and are mobile friendly with beatuful user interfaces for users to interact with.</p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">
+                                        Tab 1
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">
+                                        Tab 2
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">
+                                        Tab 3
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {
+                                            projects.map((project, index) => {
+                                                return (
+                                                    <ProjectCard
+                                                        key={index}
+                                                        {...project}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    I upload new projcts every week, contact me if you urgently need to see more of my work!
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    I upload new projcts every week contact me if you urgently need to see more of my work!
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2} alt="bg-img"></img>
+        </section>
+    )
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,87 @@
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
+
+
+export const Skills = () => {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
+    return (
+        <section className="skill" id="skills">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="skill-bx wow zoomIn">
+                            <h2>Skills</h2>
+                            <p>I continually strive to improve my web skills and learn the<br></br>latest libaries and web frameworks to build change driven projects.</p>
+                            <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                                <div className="item">
+                                    <img src={meter1} alt="Image" />
+                                    <h5>HTML/CSS</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image" />
+                                    <h5>JavaScript</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter3} alt="Image" />
+                                    <h5>Express & Nodejs</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter1} alt="Image" />
+                                    <h5>Reactjs</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image" />
+                                    <h5>Problem Solving</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image" />
+                                    <h5>AJAX & JSON</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image" />
+                                    <h5>Bootstrap</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image" />
+                                    <h5>Kanban & Scrum</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image" />
+                                    <h5>Git & Version Control</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image" />
+                                    <h5>SEO</h5>
+                                </div>
+                            </Carousel>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img className="background-image-left" src={colorSharp} alt="Image" />
+        </section>
+    )
+}
